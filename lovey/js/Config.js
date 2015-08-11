3af9e755aebe181ee7c3193c5fc50df7e3ad1a42
baoqin87 <baoqin87@sohu.com>
2015-08-11 15:54:30 +0800

    提交所有组件路径的配置

--- lovey/js/Config.js
+++ lovey/js/Config.js
@@ -1,9 +1,29 @@
 require.config({
     paths: {
         page: '../../../../webframework/lovey/js/Page',
-        input: '../../../../webframework/lovey/js/widget/form/input/InputWidget',
-        datepicker:'../../../../webframework/lovey/js/widget/form/my97date/my97DateWidget',
+        dataSet: '../../../../webframework/lovey/js/data/DataSet',
+        dataValue: '../../../../webframework/lovey/js/data/DataValue',
+        customColumns: '../../../../webframework/lovey/js/widget/customcolumns/CustomColumns',
+        customProgress: '../../../../webframework/lovey/js/widget/customprogress/CustomProgress',
+        customSearcher: '../../../../webframework/lovey/js/widget/customSearcher/CustomSearcherWidget',
+        dialog: '../../../../webframework/lovey/js/widget/dialog/DialogWidget',
         autocomplete: '../../../../webframework/lovey/js/widget/form/autocomplete/AutocompleteWidget',
-        dataSet: '../../../../webframework/lovey/js/data/DataSet'
+        checkbox: '../../../../webframework/lovey/js/widget/form/checkbox/CheckboxWidget',
+        combobox: '../../../../webframework/lovey/js/widget/form/combobox/ComboboxWidget',
+        input: '../../../../webframework/lovey/js/widget/form/input/InputWidget',
+        maskedtextbox: '../../../../webframework/lovey/js/widget/form/maskedtextbox/MaskedtextboxWidget',
+        datepicker: '../../../../webframework/lovey/js/widget/form/my97date/my97DateWidget',
+        radio: '../../../../webframework/lovey/js/widget/form/radio/RadioWidget',
+        richeditor: '../../../../webframework/lovey/js/widget/form/richeditor/RicheditorWidget',
+        slider: '../../../../webframework/lovey/js/widget/form/slider/SliderWidget',
+        switch: '../../../../webframework/lovey/js/widget/form/switch/SwitchWidget',
+        textarea: '../../../../webframework/lovey/js/widget/form/textarea/TextareaWidget',
+        tooltip: '../../../../webframework/lovey/js/widget/form/tooltip/TooltipWidget',
+        pagination: '../../../../webframework/lovey/js/widget/pagination/PaginationWidget',
+        progress: '../../../../webframework/lovey/js/widget/progress/ProgressWidget',
+        expandGrid: '../../../../webframework/lovey/js/widget/simple/expandGrid/ExpandGrid',
+        simpleGrid: '../../../../webframework/lovey/js/widget/simple/simpleGrid/SimpleGrid',
+        tree: '../../../../webframework/lovey/js/widget/tree/TreeWidget',
+        widgetContainer: '../../../../webframework/lovey/js/widget/WidgetContainer'
     }
 });
